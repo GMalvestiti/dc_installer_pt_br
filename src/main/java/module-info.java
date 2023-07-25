c596c401b80e7165252298915279bef5da3aef8d
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-07-25 01:37:01 -0300

    gson configs

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.riser876.dc_installer_pt_br {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.google.gson;
 
     opens com.riser876.dc_installer_pt_br to javafx.fxml;
     exports com.riser876.dc_installer_pt_br;
