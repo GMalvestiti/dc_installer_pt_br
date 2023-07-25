ac2cff62ee81984ad03f7f72491ab5a3a47035d5
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-07-25 01:41:02 -0300

    module info fix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.riser876.dc_installer_pt_br {
     requires javafx.fxml;
     requires com.google.gson;
 
-    opens com.riser876.dc_installer_pt_br to javafx.fxml;
+    opens com.riser876.dc_installer_pt_br to javafx.fxml, com.google.gson;
+    opens util to com.google.gson;
+    
     exports com.riser876.dc_installer_pt_br;
 }
